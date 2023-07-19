e25641322c49139ef6a0cebf19916acf2f4e2ea4
Abhinna <70975695+abhinnaofficial@users.noreply.github.com>
2023-07-20 01:30:27 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -23,21 +23,21 @@ export const fetchData = function (URL, callback) {
 export const URL = {
 
     currentWeather(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric&appid=${api_key}`;
     },
 
     forecast(lat, lon) {
 
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric&appid=${api_key}`;
     },
 
     airPollution(lat, lon) {
-        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+        return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&appid=${api_key}`;
     },
 
     reverseGeo(lat, lon) {
 
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5&appid=${api_key}`;
     },
 
     /**
@@ -45,7 +45,7 @@ export const URL = {
      */
 
     geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${api_key}`;
     }
 }
 
